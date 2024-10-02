e05c35d39e9b0a667bc921a2e67ff053677d1d8a
mikelisrael <israelipinkz@gmail.com>
2024-10-02 13:30:09 +0100

    chore: Update chat page with avatar and suspense loader

--- app/(authenticated)/chat/page.tsx
+++ app/(authenticated)/chat/page.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { SuspenseLoader } from "@/components/ui/loaders";
 
 const Chat = () => {
   return (
-    <div>Chat</div>
-  )
-}
+    <SuspenseLoader fullPage>
+      <h1 className="page-title">Chat</h1>
 
-export default Chat
+      <ul className="mt-8 divide-y">
+        {[0, 0, 0, 0].map((_, index) => {
+          return (
+            <li
+              key={index}
+              className="flex cursor-pointer gap-5 px-5 py-4 hover:bg-muted"
+            >
+              <Avatar>
+                <AvatarImage src="https://pbs.twimg.com/profile_images/1757743586349629440/Ug9EDUpk_400x400.jpg" />
+                <AvatarFallback>MI</AvatarFallback>
+              </Avatar>
+
+              <div className="flex-grow">
+                <h3 className="font-semibold">Oluwabukunmi Jeremy</h3>
+                <p className="line-clamp-1 text-sm">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laborum ipsam quod iusto harum ratione dolore velit voluptatem
+                  rem? Qui est alias deserunt perspiciatis commodi quo velit
+                  quaerat veniam id ut.
+                </p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </SuspenseLoader>
+  );
+};
+
+export default Chat;
