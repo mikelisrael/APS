05558435af8ef5b0fbd3bbcd9928ff66388a3334
mikelisrael <israelipinkz@gmail.com>
2025-08-25 22:22:43 +0100

    Update app to import svg icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
   },
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
