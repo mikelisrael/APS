71b1071bcb32390edfad633907073357493833e6
mikelisrael <israelipinkz@gmail.com>
2025-09-16 17:56:20 +0100

    Enhance image loader

--- components/shared/image-loader.tsx
+++ components/shared/image-loader.tsx
@@ -3,30 +3,42 @@
 import { cn } from "@/lib/utils";
 import Image, { ImageProps } from "next/image";
 import { useState } from "react";
+import { IoImageOutline } from "react-icons/io5";
 
-interface ImageLoaderProps extends Omit<ImageProps, "src"> {
+type ImageLoaderProps = Omit<ImageProps, "width" | "height"> & {
   className?: string;
-  src?: string;
-}
+  width?: number;
+  height?: number;
+  showLoader?: boolean;
+};
 
-const ImageLoader = ({ className, alt, src, ...props }: ImageLoaderProps) => {
+const ImageLoader = ({
+  className,
+  showLoader = true,
+  ...props
+}: ImageLoaderProps) => {
   const [hasError, setHasError] = useState(false);
 
   return (
-    <div className={cn("relative isolate overflow-hidden", className)}>
-      {!hasError && src ? (
+    <div className={cn("relative h-full w-full overflow-hidden", className)}>
+      {!hasError ? (
         <>
-          <div className="absolute inset-0 -z-10 size-full animate-pulse bg-muted" />
+          {showLoader && (
+            <div className="absolute inset-0 animate-pulse bg-gray-400" />
+          )}
           <Image
-            alt={alt}
-            src={src}
-            {...props}
-            className="size-full w-full bg-muted object-cover transition-opacity"
+            fill
+            sizes="(max-width: 1024px) 100vw, 800px"
+            className="h-full w-full object-cover transition-opacity"
             onError={() => setHasError(true)}
+            {...props}
+            alt={props.alt ?? "carousel image"}
           />
         </>
       ) : (
-        <div className="flex-center size-full bg-muted"></div>
+        <div className="bg-muted flex h-full w-full items-center justify-center">
+          <IoImageOutline className="text-muted-foreground m-auto text-2xl" />
+        </div>
       )}
     </div>
   );
