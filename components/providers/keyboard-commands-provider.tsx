4953675c697d55bc80853670ba3d3a9bc9a5364c
mikelisrael <israelipinkz@gmail.com>
2025-09-16 17:47:08 +0100

    Add keyboard command to change theme color

--- components/providers/keyboard-commands-provider.tsx
+++ components/providers/keyboard-commands-provider.tsx
@@ -1,20 +1,50 @@
 "use client";
 
+import { useAuth } from "@/hooks/use-query-resource";
+import { useTheme } from "next-themes";
 import { useRouter } from "next/navigation";
-import { PropsWithChildren, useEffect } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState
+} from "react";
+
+interface KeyboardCommandsContextType {
+  openCommandDialog: boolean;
+  setOpenCommandDialog: (open: boolean) => void;
+}
+
+const KeyboardCommandsContext = createContext<
+  KeyboardCommandsContextType | undefined
+>(undefined);
 
 const KeyboardCommandsProvider = ({ children }: PropsWithChildren) => {
   const router = useRouter();
+  const [openCommandDialog, setOpenCommandDialog] = useState(false);
+  const { logout } = useAuth();
+  const { setTheme, theme } = useTheme();
 
   const commands: { [key: string]: () => void } = {
-    "Shift+Control+P": () => router.push("/profile"),
-    "Control+S": () => router.push("/settings"),
+    "CONTROL+K": () => {
+      if (theme === "light") setTheme("dark");
+      else setTheme("light");
+    },
+    "CONTROL+/": () =>
+      setOpenCommandDialog((openCommandDialog) => !openCommandDialog),
+    "SHIFT+CONTROL+Q": () => logout(undefined)
   };
 
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key) {
-        const keyCombo = `${e.shiftKey ? "Shift+" : ""}${e.ctrlKey ? "Control+" : ""}${e.key.toUpperCase()}`;
+        const keyCombo = [
+          e.shiftKey ? "SHIFT+" : "",
+          e.ctrlKey ? "CONTROL+" : "",
+          e.key.toUpperCase()
+        ].join("");
+
         const action = commands[keyCombo];
         if (action) {
           e.preventDefault();
@@ -22,13 +52,30 @@ const KeyboardCommandsProvider = ({ children }: PropsWithChildren) => {
         }
       }
     };
+
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [router]);
+  }, [router, commands]);
 
-  return <>{children}</>;
+  return (
+    <KeyboardCommandsContext.Provider
+      value={{ openCommandDialog, setOpenCommandDialog }}
+    >
+      {children}
+    </KeyboardCommandsContext.Provider>
+  );
 };
 
 export default KeyboardCommandsProvider;
+
+export function useKeyboardCommands() {
+  const context = useContext(KeyboardCommandsContext);
+  if (!context) {
+    throw new Error(
+      "useKeyboardCommands must be used within a KeyboardCommandsProvider"
+    );
+  }
+  return context;
+}
--- components/sidebar/light-dark-switch.tsx
+++ components/sidebar/light-dark-switch.tsx
@@ -17,6 +17,9 @@ export default function LightDarkSwitch() {
         className="w-full"
       >
         Switch to {theme === "light" ? "dark" : "light"} mode
+        <kbd className="pointer-events-none ml-2 inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>K
+        </kbd>
       </Button>
     </div>
   );
