fbeb68964729c3c7df36321f93dd7add4e3cfffc
mikelisrael <israelipinkz@gmail.com>
2025-11-14 00:06:53 +0100

    Reposition community div

---
+++ app/(logged-in)/(feed-section)/communities/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CommunitiesClient from "./components/communities-client";
+
+export const metadata = {
+  title: "Communities",
+  description: "Join and engage with various communities"
+};
+
+const Communities = () => {
+  return <CommunitiesClient />;
+};
+
+export default Communities;
