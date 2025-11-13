a10688e554c18bee77ae5b5bd3cad9f6d13a7009
mikelisrael <israelipinkz@gmail.com>
2025-11-14 00:32:24 +0100

    Adjust communities layout

--- app/(logged-in)/(feed-section)/communities/components/communities-client.tsx
+++ app/(logged-in)/(feed-section)/communities/components/communities-client.tsx
@@ -1,6 +1,6 @@
 const CommunitiesClient = () => {
   return (
-    <main className="safe-area ~px-2/5">
+    <main className="~px-2/5">
       <h1 className="page-title !px-0">Communities (coming soon)</h1>
       <p>{`You'llllll love it!`}</p>
     </main>
