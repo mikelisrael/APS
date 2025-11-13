9b028a2cb19dc291d6de8a5c37238c094ee6157b
mikelisrael <israelipinkz@gmail.com>
2025-11-13 23:55:25 +0100

    Add communities page

---
+++ app/communities/components/communities-client.tsx
@@ -0,0 +1,10 @@
+const CommunitiesClient = () => {
+  return (
+    <main className="safe-area ~px-2/5">
+      <h1 className="page-title !px-0">Communities (coming soon)</h1>
+      <p>{`You'llllll love it!`}</p>
+    </main>
+  );
+};
+
+export default CommunitiesClient;
---
+++ app/communities/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'Communities',
+  description: 'Join and engage with various communities',
+}
+
+const Communities = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Communities
