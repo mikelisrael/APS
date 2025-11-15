65baf212c7e56834f44f9491e834e336086fd3ae
mikelisrael <israelipinkz@gmail.com>
2025-11-16 00:15:30 +0100

    Remove loading

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Logo from "@/public/main-logo.svg";
-
-export default function Loading() {
-  return (
-    <main className="flex h-svh w-full items-center justify-center bg-background">
-      <Logo className="size-16 text-primary" />
-    </main>
-  );
-}
