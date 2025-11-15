f78ee837a3fd0d1bb978a97eeb099076cb4519ff
mikelisrael <israelipinkz@gmail.com>
2025-11-16 00:30:37 +0100

    Add suspense loader to protected route instead

--- app/(logged-in)/layout.tsx
+++ app/(logged-in)/layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "@/components/sidebar/";
+import { SuspenseLoader } from "@/components/ui/loaders";
 
 export default function DashboardLayout({
   children
@@ -6,12 +7,14 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div
-      id="dashboardContainer"
-      className="relative mx-auto hidden h-full grid-cols-[auto,1fr] ~px-0/10 sm:max-w-5xl md:grid lg:max-w-[85rem] lg:grid-cols-[auto,1fr] xl:grid-cols-[250px,1fr]"
-    >
-      <Sidebar />
-      <div className="main-content">{children}</div>
-    </div>
+    <SuspenseLoader fullPage>
+      <div
+        id="dashboardContainer"
+        className="relative mx-auto hidden h-full grid-cols-[auto,1fr] ~px-0/10 sm:max-w-5xl md:grid lg:max-w-[85rem] lg:grid-cols-[auto,1fr] xl:grid-cols-[250px,1fr]"
+      >
+        <Sidebar />
+        <div className="main-content">{children}</div>
+      </div>
+    </SuspenseLoader>
   );
 }
